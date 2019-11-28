fc3c74685dd472bf4c2e6644fc571d3ccec3c5fa
shuhui_zhang <shuhuizhang@buaa.edu.cn>
2019-11-28 11:07:20 +0800

    保存完best model后，删除当前模型的checkpoint

--- train_multi.py
+++ train_multi.py
@@ -2,11 +2,12 @@ import os
 from utils import constant
 
 # first level:coarse classification
-os.system('CUDA_VISIBLE_DEVICES=0 python train.py --input_dropout 0.4 --type coarse')
+os.system('CUDA_VISIBLE_DEVICES=0 python train.py --input_dropout 0.5 --type coarse')
+os.system('rm -f saved_models/coarse/checkpoint_epoch_*')
 
 # second level
 for coarse_name in constant.COARSE_TO_ID:
     os.system('CUDA_VISIBLE_DEVICES=0 python train.py --input_dropout 0.4 --type multi --coarse_name ' + coarse_name)
-
+    os.system('rm -f saved_models/multi/'+coarse_name+'/checkpoint_epoch_*')
 # output result
 os.system('python result.py')
